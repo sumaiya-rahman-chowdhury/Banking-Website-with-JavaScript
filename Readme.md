917b4d7fd5f3b7879cbfd125ac2c5ec9112fecd6
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-08-14 19:01:20 -0700

    added bank.html

--- Readme.md
+++ Readme.md
@@ -1,3 +1,10 @@
 A banking website . desinged used :
 1.Tailwind CSS
 2.JavaScript
+<!--  -->
+<!--  -->
+<!--  -->
+<!--  -->
+Features :
+1.Deposit
+2.Withdraw
---
+++ login.js
@@ -0,0 +1,14 @@
+document.getElementById('btn-submit').addEventListener('click',function(){
+    // console.log("Click")
+    // step-2 : get the email address from field 
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value ;
+    // console.log(email)
+    // step-3: get password
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    console.log(password);
+    // (just check korar jnno)
+    // step-4 verify 
+    // if(email === )
+})
